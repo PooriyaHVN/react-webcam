af09363843415805c0003d3970fedf16cb43920f
pooriya peyvand <pooriyapeyvandd@gmail.com>
2020-12-07 06:09:52 +0330

    fgfg

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
 export const DEV_SITE_NAME = "http://localhost:3000/";
-export const SITE_NAME = "https://microsoft-sor.vercel.app";
+export const SITE_NAME = "https://microsoft-sor-3a3f8mc5y.vercel.app";
 export const FILE_PATH = SITE_NAME + "/public/uploads";
